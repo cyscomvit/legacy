5e5eb919869f837be66654a6d9a046a3c803207a
Atharv Agarwal <89630019+atharvagarwal@users.noreply.github.com>
2022-07-12 23:19:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Main } from './components/Main/Main';
+import { AppConfig, AppProvider } from './context/AppConfig';
+
+function App() {
+  return (
+    <div className="App">
+      <AppProvider>
+      <Main/>
+      </AppProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Members/Members.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { MemberCard } from './MemberCard'
+import { data } from '../../assets/Members/MemberData'
+import Landing from "../Landing"
+export const Members = () => {
+  return (
+    <>
+    <Landing/>
+    <div className='flex flex-row justify-center md:w-screen items-center  '>
+    <div className='flex flex-row flex-wrap justify-center'>
+    {
+data.map((item,k)=>(
+    <div key ={k}>
+        <MemberCard name = {item.name} post = {item.post} git = {item.github} link = {item.linkedin} pic = {item.pic}/>
+
+    </div>
+))
+
+    }
+    </div>
+  
+    </div>
+    <footer className="footer">
+    <p>CYSCOM VITCC | Think before you click.</p>
+    <div class="footer-links">
+      <a href="https://facebook.com/cyscomvit/"><i className="fab fa-facebook-f"  target="_blank"></i></a>
+      <a href="https://instagram.com/cyscomvit/"><i className="fab fa-instagram" target="_blank"></i></a>
+      <a href="https://twitter.com/CyscomVit/"><i className="fab fa-twitter" target="_blank"></i></a>
+      <a href="https://www.linkedin.com/company/cyscomvit/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+      <a href="https://github.com/cyscomvit"><i className="fab fa-github" target="_blank"></i></a>
+      <a href="https://discord.gg/9RyHCQgjAv"><i className="fab fa-discord" target="_blank"></i></a>
+      <a href="https://blog.cyscomvit.com/"><i className="fab fa-blogger-b" target="_blank"></i></a>
+      <a href="https://medium.cyscomvit.com/"><i className="fab fa-medium-m" target="_blank"></i></a>
+    </div>
+  </footer>
+    </>
+  )
+}
