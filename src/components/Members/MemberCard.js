36f9482cc5eb0d0fe168de755c42d462fcde388b
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2022-07-12 23:24:38 +0530

    minor bug fix

--- src/components/Members/MemberCard.js
+++ src/components/Members/MemberCard.js
@@ -11,7 +11,7 @@ export const MemberCard = (props) => {
     <div className='m-2 p-3 h-full  bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-sky-600 '>
          <div className="card w-screen h-48  middle md:h-48 md:w-48 ">
       <div className="front flex flex-row justify-center items-center">
-        <img onLoad={increment}  className={` md:max-w-[30rem] object-cover`} src={props.pic} alt=""/>
+        <img onLoad={increment}  className={` md:max-w-[30rem] object-fit`} src={props.pic} alt=""/>
       </div>
       <div class="back">
         <div class="back-content middle">
