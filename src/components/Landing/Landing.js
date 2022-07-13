cae6e04251582e3634a2b333e866af62e8bfc0aa
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2022-07-13 12:32:24 +0530

    killed bugs with style..hehe

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -13,8 +13,8 @@ const Landing = () => {
     
 
     <div className="main-div">
-   <div className="flex-divison"><h2 className="head-2">Cyscom </h2><img src="cyscom.png" style={{"width":"8vw"}}/></div>
-    <h1 className="head-1">Meet Our Legacy</h1>
+   <a href = "https://cyscomvit.com" target="_blank">  <div className="flex-divison"><h2 className="head-2">Cyscom </h2><img src="cyscom.png" style={{"width":"8vw"}}/></div></a>
+    <h1 className="head-1 hover:animate-pulse ">Meet Our Legacy</h1>
     <h3 className="head-3">Our Legacy Comprises of The Members That Crafted Some Amazing And Memorable Projects That Helped The Club and Community Grow In Several Aspects. </h3>
     <img className="main-img" src="mainCyscom.svg" alt="Image of people giving lost items"></img>
       
