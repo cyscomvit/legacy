9ae3d5715dd1fdf5d4d60f16c0219bbeec7b43bb
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2023-05-07 17:41:36 +0530

    remove errors

--- src/components/Members/MemberCard.js
+++ src/components/Members/MemberCard.js
@@ -1,30 +1,46 @@
-import React, { useContext } from 'react'
-import { AppConfig } from '../../context/AppConfig'
-import './Cards.css'
+import React, { useContext } from "react";
+import { AppConfig } from "../../context/AppConfig";
+import "./Cards.css";
 export const MemberCard = (props) => {
-    const {setnoloaded,noloaded} = useContext(AppConfig) 
-    const increment = () =>{
-        setnoloaded(noloaded+1)
-    }
+    const { setnoloaded, noloaded } = useContext(AppConfig);
+    const increment = () => {
+        setnoloaded(noloaded + 1);
+    };
     //name,post,github,link,pic
-  return (
-    <div className='m-0 p-3 h-full  bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-sky-600 '>
-         <div className="card w-screen h-48  middle md:h-48 md:w-48 ">
-      <div className="front flex flex-row justify-center items-center">
-        <img onLoad={increment}  className={` md:max-w-[30rem] object-fit`} src={props.pic} alt=""/>
-      </div>
-      <div class="back">
-        <div class="back-content middle">
-          <h2>{props.name}</h2>
-          <span> {props.post} </span>
-          <div class="sm">
-            <a href={props.link} target="_blank" rel="noreferrer" ><i class="fab fa-linkedin"></i></a>
-            <a href={props.git} target="_blank" rel="noreferrer" ><i class="fab fa-github"></i></a>
-          </div>
+    return (
+        <div className="m-0 p-3 h-full  bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-sky-600 ">
+            <div className="card w-screen h-48  middle md:h-48 md:w-48 ">
+                <div className="front flex flex-row justify-center items-center">
+                    <img
+                        onLoad={increment}
+                        className={` md:max-w-[30rem] object-fit`}
+                        src={props.pic}
+                        alt=""
+                    />
+                </div>
+                <div class="back">
+                    <div class="back-content middle">
+                        <h2>{props.name}</h2>
+                        <span> {props.post} </span>
+                        <div class="sm">
+                            <a
+                                href={props.link}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i class="fab fa-linkedin"></i>
+                            </a>
+                            <a
+                                href={props.git}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-
-    </div>
-  )
-}
+    );
+};
--- src/context/AppConfig.js
+++ src/context/AppConfig.js
@@ -1,10 +1,13 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-export const AppConfig = createContext()
-export const AppProvider = ({children}) =>{
-    const [noloaded,setnoloaded] = useState(0)
-    const loaded = noloaded>40
-    return(
-        <AppConfig.Provider value={{loaded,setnoloaded,noloaded}}> {children} </AppConfig.Provider>
-     )
-}
+export const AppConfig = createContext();
+export const AppProvider = ({ children }) => {
+    const [noloaded, setnoloaded] = useState(0);
+    const loaded = noloaded > 40;
+    return (
+        <AppConfig.Provider value={{ loaded, setnoloaded, noloaded }}>
+            {" "}
+            {children}{" "}
+        </AppConfig.Provider>
+    );
+};
